5019c37e873424416599cf6ca76531a80d23597b
Miguel E. Ruiz <meruiz77@gmail.com>
2025-02-17 01:44:07 +0100

    Add env variable for API

--- src/components/CategoryService.js
+++ src/components/CategoryService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Set your Rails API base URL here
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = process.env.REACT_APP_API_ENDPOINT;
 
 const CategoryService = {
   // Fetch all categories from the API
